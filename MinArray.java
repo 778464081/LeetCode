ab408b2ab47cf4c83461efae9a747d931af715e4
要不要买菜 <778464081@qq.com>
2020-07-22 23:04:50 +0800

    剑指Offer 11.旋转数组的最小数字

---
+++ MinArray.java
@@ -0,0 +1,17 @@
+package daily;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MinArray {
+    public int minArray(int[] numbers) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int m = (i + j) / 2;
+            if (numbers[m] > numbers[j]) i = m + 1;
+            else if (numbers[m] < numbers[j]) j = m;
+            else j--;
+        }
+        return numbers[i];
+    }
+}
